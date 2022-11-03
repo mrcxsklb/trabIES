76d13b77cc8d43d5c59d9429cf2f244ed990f8ab
mrcxsklb <marcos.kalb3@gmail.com>
2022-11-03 09:24:54 -0300

    test2

--- IES.c
+++ IES.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 int calcPotencia (int n, int x) {
     int i = 1;
@@ -17,7 +18,15 @@ int fatorial (int x) {
     return x;
 }
 
-int main () {
+float ex (int n) {
+    int i;
+    float soma = 1 + n;
+    for (i = 2; pow(n, i)/(float)fatorial(i) > 0.000001; i++)
+        soma += pow(n, i)/(float)fatorial(i);
+    return soma;    
+}
 
+int main () {
+    printf("%f ", ex(3));
     return 0;
 }
